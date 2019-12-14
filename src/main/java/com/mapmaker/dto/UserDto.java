96fbeadcbd1608abd7c81af2577640b86b9fe539
victolee <victolee93@gmail.com>
2019-12-14 13:33:40 +0900

    [MAPMAKER-3] 회원가입 유효성 체크

--- src/main/java/com/mapmaker/dto/UserDto.java
+++ src/main/java/com/mapmaker/dto/UserDto.java
@@ -19,7 +19,7 @@ public class UserDto {
     private String nickname;
 
     @NotBlank(message = "이메일은 필수 입력 값입니다.")
-    @Email(message = "이메일 형식이어야 합니다.")
+    @Email(message = "이메일 형식에 맞지 않습니다.")
     private String email;
 
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
