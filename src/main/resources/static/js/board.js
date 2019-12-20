19be9a668579d1724318e16646d554ab4d6a28ff
victolee <victolee93@gmail.com>
2019-12-20 15:55:51 +0900

    [MAPMAKER-4] 게시판 - JS 모던화, 모달 수정

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -1,66 +1,45 @@
-var writeBtn = document.querySelector('#board-write-btn');
-var updateBtn = document.querySelector('#board-update-btn');
 
-var writeSubmitBtn = document.querySelector('#write-submit-btn');
-var writeCancelBtn = document.querySelector('#write-cancel-btn');
+boardObj = {
+    
+    init : () => {
+        boardObj.modalEventRegister();
 
-var updateSubmitBtn = document.querySelector('#update-submit-btn');
-var updateCancelBtn = document.querySelector('#update-cancel-btn');
+        boardObj.submit();
+    },
 
-/*
- * 함수 정의
- */
-var openWriteModal = function() {
-    $('.ui.modal.write').modal('show');
+    /*
+     * 모달 이벤트 등록
+     */
+    modalEventRegister : () => {
+        const writeHiddenElement = document.querySelector(".hidden");
+
+        // modal show
+        document.querySelector("#write-btn").addEventListener("click", () => {
+            writeHiddenElement.classList.remove('hidden');
+
+            if (validatorObj.isEmpty(title) === false && validatorObj.isEmpty(content) === false) {
+                document.querySelector('[name="title"]').value = title;
+                document.querySelector('[name="content"]').value = content;
+            }
+        });
+
+        // modal hidden
+        document.querySelector("#cancel-btn").addEventListener('click', ()=> {
+            writeHiddenElement.classList.add('hidden');
+        });
+        document.querySelector(".modal_overlay").addEventListener('click', ()=> {
+            writeHiddenElement.classList.add('hidden');
+        });
+    },
+
+    /*
+     * form 전송
+     */
+    submit : () => {
+        document.querySelector("#submit-btn").addEventListener("click", () => {
+            this.closest('form').submit();
+        })
+    }
 };
 
-var openUpdateModal = function() {
-    $('.ui.modal.update').modal('show');
-
-    document.querySelector('[name="title"]').value = title;
-    document.querySelector('[name="content"]').value = content;
-};
-
-var writeFormSubmit = function() {
-    document.querySelector('#write-form').submit();
-};
-
-var writeFormCancel = function() {
-    $('.ui.modal.write').modal('hide');
-};
-
-var updateSubmit = function() {
-    document.querySelector('#update-form').submit();
-};
-
-var updateCancel = function() {
-    $('.ui.modal.update').modal('hide');
-};
-
-
-/*
- * 이벤트 등록
- */
-if (writeBtn) {
-    writeBtn.addEventListener('click', openWriteModal);
-}
-
-if (updateBtn) {
-    updateBtn.addEventListener('click', openUpdateModal);
-}
-
-if (writeSubmitBtn) {
-    writeSubmitBtn.addEventListener('click', writeFormSubmit);
-}
-
-if (writeCancelBtn) {
-    writeCancelBtn.addEventListener('click', writeFormCancel);
-}
-
-if (updateSubmitBtn) {
-    updateSubmitBtn.addEventListener('click', updateSubmit);
-}
-
-if (updateCancelBtn) {
-    updateCancelBtn.addEventListener('click', updateCancel);
-}
+boardObj.init();
